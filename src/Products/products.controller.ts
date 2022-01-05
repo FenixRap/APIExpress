830fb553898f6971b8b59a8de4fa429ad0528f20
Monarque <fenixwhy@gmail.com>
2022-01-05 22:50:10 +0300

    123

--- src/Products/products.controller.ts
+++ src/Products/products.controller.ts
@@ -27,10 +27,42 @@ export class ProductsController extends BaseController implements IProductsContr
     }
 
      async createItem(req:Request, res: Response, next: NextFunction):Promise<void>{
+         const {
+                 name, discountPrice, price, category,
+                 active1, active2, active3, active4,
+                 active5, active6, volume, count,
+                 sellCount, barCode, engName, skinType,
+                 madeCountry, howToUse, description, type,
+                 brand, rating
+                } = req.body
 
+             const fileName1 = uuid() + ".jpg"
+             const fileName2 = uuid() + ".jpg"
+             const fileName3 = uuid() + ".jpg"
 
-        const result = await this.ProductsService.createProduct(req.body)
-        this.ok(res,"Пиздец. Ты это сделал.")
+             let img:any = req.files;
+             let img2:any = req.files;
+             let img3:any = req.files;
+
+             img.mv(path.resolve(__dirname, '..', 'static', fileName1))
+             img2.mv(path.resolve(__dirname, '..', 'static', fileName2))
+             img3.mv(path.resolve(__dirname, '..', 'static', fileName3))
+
+             img = fileName1;
+             img2 = fileName2;
+             img3 = fileName3;
+
+              const item = {
+                  name, discountPrice, price, img,
+                  img2, img3, category, active1,
+                  active2, active3, active4, active5,
+                  active6, volume, count, sellCount,
+                  barCode, engName, skinType, madeCountry,
+                  howToUse, description, type, brand, rating
+              }
+
+        const result = await this.ProductsService.createProduct(item)
+        this.ok(res,req.body)
     }
 
 
